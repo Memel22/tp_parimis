d8848fe9d2d6433b321748766ce9d496890c72a8
Memel22 <ameliecave@yahoo.fr>
2022-02-19 22:53:29 +0100

    add all files

--- app.js
+++ app.js
@@ -17,21 +17,3 @@ function closeNav() {
 }
 
 
-/* ------------------------- DIAPO -------------------------*/ 
-$( function() {
-    let myIndex = 0;
-       carousel();
-       function carousel() {
-           let i;
-           let x = document.getElementsByClassName("mySlides");
-           for (i = 0; i < x.length; i++) {
-               x[i].style.display = "none";
-           }
-           myIndex++;
-           if (myIndex > x.length) {
-               myIndex = 1
-           }
-           x[myIndex - 1].style.display = "block";
-           setTimeout(carousel, 4000);
-       }
-     } );
